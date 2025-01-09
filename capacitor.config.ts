d7ce5d3b3d6eccafeff3aab0f62d045de1cdea3e
FIR3xd <95378281+FIR3xd@users.noreply.github.com>
2025-01-09 11:59:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'countries-finder',
+  webDir: 'www'
+};
+
+export default config;
